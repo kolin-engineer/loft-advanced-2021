c771afc8806401b2b9461a7c49aa9e117499d26e
kolin-engineer <nickolai.zt@gmail.com>
2021-06-25 15:23:16 +0300

    Finished portfolio slider

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,5 +1,15 @@
 import Vue from "vue";
 
+const SliderNav = {
+  template: "#t-slider-nav",
+  emits: ["change-slide"],
+  methods: {
+    changeSlide(direction) {
+      this.$emit("change-slide", direction);
+    },
+  },
+};
+
 const SliderItem = {
   template: "#t-slider-item",
   props: {
@@ -7,6 +17,29 @@ const SliderItem = {
   },
 };
 //
+const SliderThumbItem = {
+  template: "#t-slider-thumb-item",
+  props: {
+    filename: String,
+    id: String | Number,
+    showImgId: String | Number,
+  },
+};
+//
+const SliderThumbList = {
+  template: "#t-slider-thumb-list",
+  components: {
+    SliderThumbItem,
+  },
+  emits: ["set-slide"],
+  props: ["gallery", "showImgId"],
+  methods: {
+    setSlide(id) {
+      this.$emit("set-slide", id);
+    },
+  },
+};
+
 const SliderList = {
   name: "SliderList",
   template: "#t-slider-list",
@@ -19,7 +52,27 @@ const SliderList = {
   },
 };
 //
+const TagItem = {
+  name: "TagItem",
+  template: `<div class='tag'>{{ tag }}</div>`,
+  props: ["tag"],
+};
+const SliderDesc = {
+  template: "#t-slider-desc",
+  props: ["portfolio", "showWorkId"],
+  components: {
+    TagItem,
+  },
+};
+
 const SliderPreview = {
+  template: "#t-slider-preview",
+  components: {
+    SliderList,
+    SliderThumbList,
+    SliderNav,
+    SliderDesc,
+  },
   props: {
     portfolio: Array,
   },
@@ -49,21 +102,40 @@ const SliderPreview = {
         console.warn(err);
       }
     },
+    changeSlide(direction) {
+      let newIndex = this.currentImgIndex + direction;
+      if (newIndex >= this.gallery.length) {
+        this.currentImgIndex = newIndex - this.gallery.length;
+      } else if (newIndex < 0) {
+        this.currentImgIndex = this.gallery.length + newIndex;
+        // do:
+      } else {
+        this.currentImgIndex = newIndex;
+      }
+    },
+    setSlide(id) {
+      this.currentImgId = id;
+    },
   },
   computed: {
+    currentImgIndex: {
+      set: function(index) {
+        this.currentImgId = this.gallery[index].id;
+      },
+      get: function() {
+        return this.gallery.findIndex((el) => el.id === this.currentImgId);
+      },
+    },
     currentImgOrder() {
-      return 1 + this.gallery.findIndex((el) => el.id === this.currentImgId);
+      return 1 + this.currentImgIndex;
     },
   },
   created() {
     this._initGallery();
     this._initCurrentImgId();
   },
-  template: "#t-slider-preview",
-  components: {
-    SliderList,
-  },
 };
+
 new Vue({
   el: "#slider-widget",
   template: "#t-slider",
@@ -83,7 +155,13 @@ new Vue({
       } catch (err) {
         console.warn(err || "Couldn't get data from " + dbName);
       }
-      this.portfolio = portfolioJSON.portfolio;
+      this.portfolio = this._resolveImgsPath(portfolioJSON.portfolio);
+    },
+    _resolveImgsPath(dataArr) {
+      dataArr.map((item) => {
+        item.img = require("../images/content/" + item.img).default;
+      });
+      return dataArr;
     },
   },
   created() {
